7e7f50a366a09afa144527ab953799f6c0a3c81e
kyuss <kyuss@google.com>
2023-11-02 00:13:20 +0300

    add celery-beats

--- django-backend/src/proj/settings.py
+++ django-backend/src/proj/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
     'django_extensions',
     'rest_framework',
     'scrapper.apps.ScrapperConfig',
